01a411e787a4cbfdd9e4ff95fb921e6cf43fff13
akshayaraojjs <akshayarao.parvam@gmail.com>
2026-04-01 13:29:49 +0530

    Completed CRUD Operation in Django

--- Backend/Django/myproject/student_app/urls.py
+++ Backend/Django/myproject/student_app/urls.py
@@ -4,5 +4,7 @@ from .import views
 urlpatterns = [
     path('', views.student_list, name="list_of_students"),
     path('form/', views.student_form, name="registration_form"),
-    path('info/', views.student_info, name="student_details"),
+    path('<int:pk>/', views.student_info, name="student_details"),
+    path('<int:pk>/edit', views.edit_student, name="update_details"),
+    path('<int:pk>/delete', views.delete_student, name="remove_student"),
 ]
